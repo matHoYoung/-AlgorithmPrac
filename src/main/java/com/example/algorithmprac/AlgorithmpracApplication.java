f2afcd502ab79034825860d8bc4be4c1a5fb96df
matHoYoung <hy9825@naver.com>
2022-11-15 15:04:37 +0900

    세균증식

---
+++ src/main/java/com/example/algorithmprac/AlgorithmpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithmprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithmprac/AlgorithmpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithmprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
